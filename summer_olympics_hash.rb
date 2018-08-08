098b398f7b0e2587e0559a7a7e23ab0c0cb21509
Leyla Alekperova <leslairy@gmail.com>
2018-08-08 17:43:04 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,11 +1,20 @@
+hash = {
+  :Sydney => "2000",
+  :Athens => "2004",
+  :Beijing => "2008", 
+  :London => "2012" }
 
-def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
-end
 
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
+hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012" }
+hash[:Atlanta] = "1996"
+
+puts hash
+
+
+
+# add_a_key_value_pair
+# create_olympics_hash.each do |country_name,year|
+#   puts "country:#{}"
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
