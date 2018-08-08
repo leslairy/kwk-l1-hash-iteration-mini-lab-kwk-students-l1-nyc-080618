37ff3e4d559f2504e15778f8604f1b063e4d5589
Leyla Alekperova <leslairy@gmail.com>
2018-08-08 17:23:19 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,4 +1,4 @@
-create_olympics_hash = {"Sydney" => 2000, "Athens"=> 2004, "Beijing"=> 2008, "London" => 2012 }
+create_olympics_hash = {"Sydney" => 2000, "Athens" => 2004, "Beijing" => 2008, "London" => 2012 }
 puts create_olympics_hash
 
 def create_olympics_hash
