74628f602a9cd961bcdacaa70cd2d04386f395b1
Leyla Alekperova <leslairy@gmail.com>
2018-08-08 17:32:45 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -11,10 +11,10 @@ def create_olympics_hash
   :Sydney => "2000",
   :Athens => "2004",
   :Beijing => "2008", 
-  :London => "2012" 
-}
- 
-end
+  :London => "2012" }
+ end
+
+create_olympics_hash
 
 def add_a_key_value_pair
   
